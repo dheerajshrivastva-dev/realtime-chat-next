5bef46377aabc20d6255a7bc4855b87a2b6db18c
Dheeraj Shrivastva <dheeraj.srivastava@iesoftek.com>
2024-06-30 00:32:28 +0530

    fix dark mode and some layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import { FC, useState } from 'react'
 import { Icons } from '../components/Icons';
-import Button from '../components/ui/Button';
+import Button, { buttonVariants } from '../components/ui/Button';
 import { signIn } from 'next-auth/react';
 import GoogleIcon from '../components/ui/googleIcon';
+import { MoveLeft } from 'lucide-react';
+import Link from 'next/link';
 
 interface pageProps {
   
@@ -23,8 +25,14 @@ const Login: FC<pageProps> = ({}) => {
   }
   return (
     <>
-      <div className='flex justify-center items-center py-12 px-4 sm:px-6 lg:px-8'>
-        <div className='w-full flex flex-col items-center max-w-md space-y-8'>
+      <div className='flex justify-center py-12 px-4 sm:px-6 lg:px-8'>
+       
+        <div className='w-full flex flex-col items-center max-w-md space-y-8 relative'>
+          <div className='absolute top-0 left-0'>
+            <Link href={"/"} className={buttonVariants({variant: "ghost"})}>
+              <MoveLeft className='h-4 w-4' />
+            </Link>
+          </div>
           <div className='flex flex-col items-center gap-8'>
             <Icons.Logo className='h-10 w-auto text-pink-600'/>
             <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-300'>Sign in to your account</h2>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
 'use client'
-import Button from "@/app/components/ui/Button";
+import Button, { buttonVariants } from "@/app/components/ui/Button";
 import Loader from "@/app/components/ui/LoaderIos";
-import { WaveLoader } from "@/app/components/ui/AllLoaders";
+import Link from "next/link";
 import { ChangeEvent, useState } from "react";
 import { TwitterPicker } from "react-color";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [size, setSize] = useState<number | string>(200);
-  const [speed, setSpeed] = useState<number | string>(1);
+  const [speed, setSpeed] = useState<number | string>(10);
   const [selectedColor, setSelectedColor] = useState<string>('#000000');
   const [selectedBgColor, setSelectedBgColor] = useState<string>('#737373'); 
   return (
-    <div className="container mx-auto px-5 py-6">
+    <div className="">
+      <div className="px-5">
       <div className="flex flex-row items-center gap-4 my-4">
         <label className="w-[100px]" htmlFor="size">Size</label>
         <input type="range" id="size" value={size} min={10} max={500} className="bg-black text-white rounded py-2 px-2 border-2 border-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50" onInput={(e: ChangeEvent<HTMLInputElement>) => {
@@ -28,7 +29,7 @@ export default function Home() {
         }}></input>
       </div>
 
-      <div className="flex flex-row items-center gap-4 my-4">
+      <div className="flex flex-col md:flex-row md:items-center gap-4 my-4">
         <div className="flex flex-row w-50 items-center gap-4">
 
           <label className="w-[100px]" htmlFor="Color">Color</label>
@@ -53,7 +54,12 @@ export default function Home() {
           <Button size="default" onClick={() => setIsLoading(a => !a)}>{isLoading ? "Stop" : "Start"}</Button>
         </label>
         <Loader isLoading={isLoading} size={Number(size)} speed={Number(speed)} color={selectedColor} bgColor={selectedBgColor}/>
-        <WaveLoader />
+      </div>
+      </div>
+      <div className="absolute bottom-10 w-full px-5">
+        <Link href={"/login"} className={buttonVariants({variant: "default", className: "w-full p-0 m-0"})}>
+          Login/Signup
+        </Link>
       </div>
     </div>
   );
